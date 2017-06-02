97847fa1aba7c784e1b95d8771f0310fd80e49f9
shivatiwari <shivansh22t@gmail.com>
2017-06-02 20:18:30 +0530

    Update sketch5.js

--- sketch5.js
+++ sketch5.js
@@ -29,7 +29,7 @@ function draw()
     fill(col.r,col.g,col.b,200);
     ellipse (spot.x, spot.y, 25, 25);
     
-    if(mouseIsPressed)
+     if(mouseIsPressed)
     {
     fill(0);
     ellipse(mouseX,mouseY,100,100);
