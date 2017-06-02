8b07269e92f01a9d0172ee3a597d264943f0f774
shivatiwari <shivansh22t@gmail.com>
2017-06-02 20:11:50 +0530

    Create sketch5.js

---
+++ sketch5.js
@@ -0,0 +1,35 @@
+var spot ={
+    x: 100,
+    y: 50
+
+}
+
+var col = {
+    r:255,
+    g:0,
+    b:0
+}
+
+function setup()
+{
+  createCanvas(innerWidth,innerHeight); 
+    background(0);
+}
+
+function draw()
+{
+    col.r = random(100, 255);
+    col.g = 0;
+    col.b = random (100,255);
+    
+    spot.x = random (0,width);
+    spot.y = random (0,height);
+    
+    noStroke();
+    fill(col.r,col.g,col.b,200);
+    ellipse (spot.x, spot.y, 25, 25);
+    
+    fill(0);
+    ellipse(mouseX,mouseY,100,100);
+  
+}
